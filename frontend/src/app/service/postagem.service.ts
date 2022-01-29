58fc338115406c935739b8bf4b7721f4090b7830
Jhon Med <jhonatan1medeiros@gmail.com>
2022-01-28 22:25:47 -0300

    CRUD completo da model Postagens

---
+++ frontend/src/app/service/postagem.service.ts
@@ -0,0 +1,21 @@
+import { environment } from './../../environments/environment.prod';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Postagem } from '../model/Postagem';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostagemService {
+
+  constructor(private http: HttpClient) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+
+  getAllPostagens () : Observable<Postagem[]>{
+    return this.http.get<Postagem>('')
+  }
+
+}
